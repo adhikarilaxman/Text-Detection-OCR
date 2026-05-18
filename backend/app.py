10bb178a4f6e3226b06f9a5f5ae85c3ffe78c848
Laxman Adhikari <adhikarilaxman441@gmail.com>
2026-05-18 16:42:04 +0530

    Initial commit

--- backend/app.py
+++ backend/app.py
@@ -45,7 +45,9 @@ def create_app():
     
     return app
 
+
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     # Run development server explicitly on port 5000
     app.run(debug=True, port=5000)
